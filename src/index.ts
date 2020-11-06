3d704245482b3d2d14e40c5bf5df0c6001915c7a
Tim Lantz <tlantz@gmail.com>
2020-11-06 13:19:30 -0600

    add basic User class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('Yo!');
+import { User } from './models/User';
+
+const user = new User({name: 'Hannibal', age: 45});
+user.set({age: 666});
+
+console.log(user.get('name'));
+console.log(user.get('age'));
