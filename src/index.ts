87223680310dc49a4868183f8b7d8ac1e0b0892a
Tim Lantz <tlantz@gmail.com>
2020-11-10 16:58:59 -0600

    add save and fetch to User

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({name: 'Hannibal', age: 45});
+const user = new User({ name: 'Will Graham', age: 33 });
 
-user.on('change', () => {
-  console.log('changed!');
-});
-
-user.on('flex', () => {
-  console.log('Flexxed!!!!');
-});
-
-user.trigger('change');
-user.trigger('flex');
-user.trigger('flim');
+user.save();
