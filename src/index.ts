f60828ab5de41a7bde0e830ace07436ed6fb7070
Tim Lantz <tlantz@gmail.com>
2020-11-11 14:39:19 -0600

    implement accessors on User

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'Will Graham', age: 33 }); 
+// const user = new User({name: 'Will', age: 33});
+
+// user.on('save', () => {
+//   console.log(user);
+// })
+
+// user.save();
