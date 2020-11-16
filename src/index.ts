c5567c4ff0be9739fa4f0fc4fbd476de27a3e9df
Tim Lantz <tlantz@gmail.com>
2020-11-16 16:41:44 -0600

    update render

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
-const userForm = new UserForm(document.getElementById('root'));
+const user = User.buildUser({name: 'Frodo', age: 300})
 
-userForm.render();
+const root = document.getElementById('root');
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error('Root element not found!');
+}
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,27 +1,44 @@
+import { User } from '../models/User';
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel()
+  }
+
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render();
+    })
+  }
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClick,
-      'mouseenter:h1': this.onHeaderHover
+      'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick
     };
   }
 
-  onButtonClick(): void {
-    console.log('Yo!');
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
   }
 
-  onHeaderHover(): void {
-    console.log('H1 was hovered upon!');
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+    if (input) {
+      const name = input.value;
+      this.model.set({ name });
+    }
   }
 
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
+        <div>User name: ${this.model.get('name')}</div>
+        <div>Age: ${this.model.get('age')}</div>
         <input />
-        <button>Submit</button>
+        <button class="set-name">Update Name</button>
+        <button class="set-age">Set Random Age</button>
       </div>
     `;
   }
@@ -31,7 +48,6 @@ export class UserForm {
 
     for (let eventKey in eventsMap) {
       const [eventName, selector] = eventKey.split(':');
-
       fragment.querySelectorAll(selector).forEach((element: Element) => {
         element.addEventListener(eventName, eventsMap[eventKey]);
       })
@@ -39,6 +55,8 @@ export class UserForm {
   }
 
   render(): void {
+    this.parent.innerHTML = '';
+
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
 
