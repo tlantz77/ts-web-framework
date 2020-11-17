c535a081db3344b6cdfe6677a03a8765481f1879
Tim Lantz <tlantz@gmail.com>
2020-11-16 18:43:15 -0600

    add user collection views

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+import { User, UserProps } from './models/User';
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
 
-const user = User.buildUser({name: 'user', age: 0})
+const defaultUser = User.buildUser({name: 'user', age: 0});
+const users = new Collection('http://localhost:3000/users', (json: UserProps) => {
+  return User.buildUser(json);
+});
 
-const root = document.getElementById('root');
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
-  console.log(userEdit);
-} else {
-  throw new Error('Root element not found!');
-}
+users.on('change', () => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+})
+
+users.fetch();
